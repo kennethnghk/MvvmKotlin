c3a4408a68bd170811b1aec1362015a73fe78f60
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-08-10 17:35:23 +0800

    Integrate the viewModel into the ListFragment

--- app/src/main/java/im/tobe/mvvmkotlin/view/ListFragment.kt
+++ app/src/main/java/im/tobe/mvvmkotlin/view/ListFragment.kt
@@ -5,9 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import im.tobe.mvvmkotlin.R
+import im.tobe.mvvmkotlin.model.Animal
+import im.tobe.mvvmkotlin.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+    private val listAdapter = AnimalListAdapter(arrayListOf())
+
+    private val animalListDataObserver = Observer<List<Animal>> { list ->
+        // list? means if list is not null, then implement
+        list?.let {
+            animalList.visibility = View.VISIBLE
+            listAdapter.updateAnimalList(it)
+        }
+    }
+
+    private val isLoadingLiveDataObserver = Observer<Boolean> { isLoading ->
+        loadingView.visibility = if (isLoading) View.VISIBLE else View.GONE
+        if (isLoading) {
+            listError.visibility = View.GONE
+            animalList.visibility = View.GONE
+        }
+    }
+
+    private val errorLiveDataObserver = Observer<Boolean> { isError ->
+        listError.visibility = if (isError) View.VISIBLE else View.GONE
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +44,20 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this)[ListViewModel::class.java]
+        viewModel.animals.observe(viewLifecycleOwner, animalListDataObserver)
+        viewModel.isloading.observe(viewLifecycleOwner, isLoadingLiveDataObserver)
+        viewModel.loadError.observe(viewLifecycleOwner, errorLiveDataObserver)
+        viewModel.refresh()
+
+        animalList.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = listAdapter
+        }
     }
 }
