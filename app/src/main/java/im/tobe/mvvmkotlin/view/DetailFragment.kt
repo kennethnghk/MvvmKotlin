fcf094691fcb34172ed9240a178bba55d310862f
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-08-16 18:35:34 +0800

    Show background color according to animal image

--- app/src/main/java/im/tobe/mvvmkotlin/view/DetailFragment.kt
+++ app/src/main/java/im/tobe/mvvmkotlin/view/DetailFragment.kt
@@ -1,10 +1,16 @@
 package im.tobe.mvvmkotlin.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import im.tobe.mvvmkotlin.R
 import im.tobe.mvvmkotlin.model.Animal
 import im.tobe.mvvmkotlin.util.getProgressDrawable
@@ -38,5 +44,24 @@ class DetailFragment : Fragment() {
         animalLocation.text = animal?.location
         animalLifeSpan.text = animal?.lifeSpan
         animalDiet.text = animal?.diet
+
+        animal?.imageUrl?.let {
+            setBackgroundColor(it)
+        }
+    }
+
+    private fun setBackgroundColor(url: String) {
+        Glide.with(this).asBitmap().load(url).into(object: CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                Palette.from(resource).generate() { pallete ->
+                    var intColor = pallete?.lightMutedSwatch?.rgb ?: 0
+                    animalDetailLayout.setBackgroundColor(intColor)
+                }
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
